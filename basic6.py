437d02819ed79537e71e9c6f4fe04465d9c8bde1
SangDuck <phb1247@naver.com>
2021-03-26 09:47:04 +0900

    테스트

--- basic6.py
+++ basic6.py
@@ -1,6 +1,13 @@
-n = str(input())
-n = int(n , 16) 
+n1, n2 , n3 = input().split()
 
-for i in range(1,16 ) :
-    print('%X'%n, '*%X'%i, '=%X'%(n*i), sep='')
-ddddfdfadfass
+for i in range(1, n+1) :
+    if i%10==3 :
+        print("X", end=' ')
+    else :
+        if i%10==6 :
+            print("X", end=' ')
+        else :
+            if i%10==9 :
+                print("X", end=' ')
+            else :
+                print(i, end=' ')
