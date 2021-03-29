63495ee1afdb298573bab97fc9facdef7f8f50f1
SangDuck <phb1247@naver.com>
2021-03-29 21:51:32 +0900

    21.03.29

--- basic6.py
+++ basic6.py
@@ -1,13 +1,9 @@
-n1, n2 , n3 = input().split()
+n1 , n2 , n3 = input().split()
 
-for i in range(1, n+1) :
-    if i%10==3 :
-        print("X", end=' ')
-    else :
-        if i%10==6 :
-            print("X", end=' ')
-        else :
-            if i%10==9 :
-                print("X", end=' ')
-            else :
-                print(i, end=' ')
+c = 0
+for i in range(n1, n3) :
+    if c>
+    if i%3==0 :
+        continue  
+          #다음 반복 단계로 넘어간다.
+    print(i, end=' ')    #i가 짝수가 아닐 때만 실행된다.
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")print("yffffgfgfg")
+n1 , n2 , n3 = input().split()
+
+c = int(n1)
+for i in range(int(n1), int(n3)) :
+    c += int(n2)
+print(c)    
